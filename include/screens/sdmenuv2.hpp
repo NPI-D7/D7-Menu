968ed1d0e51db416f1562eeb234d6836ede07b65
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-09-25 18:45:24 +0200

    Add files via upload

---
+++ include/screens/sdmenuv2.hpp
@@ -0,0 +1,43 @@
+#include "common.hpp"
+#include "structs.hpp"
+
+#include "gameManagement.hpp"
+
+#include <vector>
+
+#define ENTRIES_PER_SCREENN
+
+class SDMenu : public Screen {
+public:
+	void Draw(void) const override;
+	void Logic(u32 hDown, u32 hHeld, touchPosition touch) override;
+private:
+
+	
+	int selectedTitle = 0;
+	bool GameSelected   = false;
+	int maxTitles;
+
+	mutable int screenPos = 0;
+	mutable int screenPosList = 0;
+	mutable int selection = 0;
+	int Selection = 0;
+	std::vector<Structs::ButtonPos> mainButtons = {
+		{10, 40, 48, 48}, // Button 1.
+		{10, 100, 48, 48}, // Button 2.
+		{10, 160, 48, 48}, // Button 3.
+       
+	};
+    std::shared_ptr<TitleManagement> titleFromIndex(int i) const
+	{
+		if (i == -2)
+		{
+			return nullptr;
+		}
+		else if ((size_t)i < GameManagement::installedTitles.size())
+		{
+			return GameManagement::installedTitles[i];
+		}
+		return nullptr;
+	}
+};
