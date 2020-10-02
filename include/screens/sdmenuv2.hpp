191ef82f105e7bc77b7462b60d1d43210fc7e26f
Tobi-D7 <71647024+Tobi-D7@users.noreply.github.com>
2020-10-02 21:06:46 +0000

    Update sdmenuv2.hpp

--- include/screens/sdmenuv2.hpp
+++ include/screens/sdmenuv2.hpp
@@ -11,7 +11,7 @@ class SDMenu : public Screen {
 public:
 	void Draw(void) const override;
 	void Logic(u32 hDown, u32 hHeld, touchPosition touch) override;
-        void SDMenu();
+        SDMenu();
 private:
 
 	
